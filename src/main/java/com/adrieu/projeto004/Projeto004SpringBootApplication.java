00f2ba9464d808baf214ea7e88038e703c1f1bcc
AdrieuPazin <adrieu.pazin@gmail.com>
2021-05-16 15:45:02 -0300

    Projeto Criado

---
+++ src/main/java/com/adrieu/projeto004/Projeto004SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.adrieu.projeto004;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projeto004SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projeto004SpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adrieu/projeto004/Projeto004SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrieu.projeto004;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projeto004SpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
