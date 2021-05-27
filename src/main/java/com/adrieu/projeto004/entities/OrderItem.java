fed939f2be023e8de5170d1275552ec888dbd196
AdrieuPazin <adrieu.pazin@gmail.com>
2021-05-26 23:04:26 -0300

    Feito metodo subtotal e Total

--- src/main/java/com/adrieu/projeto004/entities/OrderItem.java
+++ src/main/java/com/adrieu/projeto004/entities/OrderItem.java
@@ -65,6 +65,11 @@ public class OrderItem implements Serializable {
 		this.preco = preco;
 	}
 
+	public Double getSubTotal() {
+		return preco*quantidade;
+	}
+	
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
